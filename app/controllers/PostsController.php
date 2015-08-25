18f4eb50d558bc1923a5640a4eecca07455cd9a2
Ben Batschelet <ben@codeup.com>
2015-08-25 18:23:34 -0500

    Consolidate store() and update() into validateAndSave()

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -53,34 +53,13 @@ class PostsController extends \BaseController {
 	 *
 	 * @return Response
 	 */
-	public function store()
+    public function store()
     {
-        // create the validator
-        $validator = Validator::make(Input::all(), Post::$rules);
+        $post = new Post();
 
-        // attempt validation
-        if ($validator->fails()) {
+        $post->user()->associate(Auth::user());
 
-            Session::flash('errorMessage', 'Something went wrong. Please read errors below:');
-
-            Log::info('Validator failed', Input::all());
-
-            return Redirect::back()->withInput()->withErrors($validator);
-
-        } else {
-            // validation succeeded, create and save the post
-            $post = new Post;
-            $post->title = Input::get('title');
-            $post->body = Input::get('body');
-            $post->user_id = Auth::id();
-            $post->save();
-
-            Log::info("Post successfully saved.", Input::all());
-
-            Session::flash('successMessage', 'You created ' . $post->title . ' post successfully');
-
-            return Redirect::action('PostsController@index');
-        }
+        return $this->validateAndSave($post);
     }
 
 
@@ -134,37 +113,18 @@ class PostsController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
-	{
-		// create the validator
-        $validator = Validator::make(Input::all(), Post::$rules);
+    public function update($id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            Session::flash('errorMessage', "Post with id of $id is not found");
 
-        // attempt validation
-        if ($validator->fails()) {
-            return Redirect::back()->withInput()->withErrors($validator);
-            // validation failed, redirect to the post create page with validation errors and old inputs
-        } else {
-            // validation succeeded, create and save the post
-            $post = Post::find($id);
-
-            if(!$post) {
-                $message = "Post with id of $id is not found";
-
-                Log::warning($message);
-
-                Session::flash('errorMessage', $message);
-
-                App::abort(404);
-            }
-
-            $post->title = Input::get('title');
-            $post->body = Input::get('body');
-            $post->save();
-
-            return Redirect::action('PostsController@index');
+            App::abort(404);
         }
-	}
 
+        return $this->validateAndSave($post);
+    }
 
 	/**
 	 * Remove the specified resource from storage.
@@ -189,5 +149,30 @@ class PostsController extends \BaseController {
         return Redirect::action('PostsController@index');
 	}
 
+    protected function validateAndSave(Post $post)
+    {
+        // create the validator
+        $validator = Validator::make(Input::all(), Post::$rules);
 
+        // attempt validation
+        if ($validator->fails()) {
+            Session::flash('errorMessage', 'Failed to save the post, sorry.');
+
+            Log::info('Validator failed', Input::all());
+
+            return Redirect::back()->withInput()->withErrors($validator);
+        } else {
+            // validation succeeded, create and save the post
+            $post->title = Input::get('title');
+            $post->body  = Input::get('body');
+
+            $post->save();
+
+            Log::info('Post successfully saved.', Input::all());
+
+            Session::flash('successMessage', 'Post "' . $post->title . '" saved!');
+
+            return Redirect::action('PostsController@show', $post->id);
+        }
+    }
 }

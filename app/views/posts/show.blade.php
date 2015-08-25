95d1b8c942f5e4b79a73b0c728148975b527de8b
Ben Batschelet <ben@codeup.com>
2015-08-25 18:26:36 -0500

    Semantic tagging for show and index
    What the hell was the pager doing at the top?!
    Spit out some dates as well

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -1,45 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+    <header class="page-header">
+        <h1>{{{ $post->title }}}</h1>
+        <small class="text-muted">
+            {{{ $post->created_at->format('F jS, Y g:i a') }}}
+            by {{{ $post->user->first_name }}} {{{ $post->user->last_name }}}
+        </small>
+    </header>
 
-    <h2>{{{ $post->title }}}</h2>
-    <p>{{ $post->user->first_name }}</p>
     <p>{{{ $post->body }}}</p>
 
-    @if(Session::has('test'))
-        {{ Session::get('test') }}
-    @endif
-
-    <div class="row">
-        <div class="col-md-6">
-            <a class="btn btn-primary" href="{{ action('PostsController@edit', $post->id) }}">Edit</a>
+    @if (Auth::check())
+        <footer>
+            <a class="btn btn-default" href="{{ action('PostsController@edit', $post->id) }}">Edit</a>
             <button id="delete" class="btn btn-danger">Delete</button>
-        </div>
-    </div>
+        </footer>
+    @endif
 
     {{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE', 'id' => 'formDelete')) }}
     {{ Form::close() }}
-
 @stop
 
 @section('script')
     <script>
-
         (function(){
-
             "use strict";
 
             $('#delete').on('click', function(){
-
-                var onConfirm = confirm('Are you sure you want to. There is no turning back!');
-
-                if(onConfirm) {
+                if(confirm('Are you sure you want to do this? There is no turning back!')) {
                     $('#formDelete').submit();
                 }
-
             });
-
         })();
-
     </script>
 @stop

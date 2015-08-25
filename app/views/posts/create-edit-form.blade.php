3df1d25e0365dd9934d6ee20b5c52236fd63fec5
Ben Batschelet <ben@codeup.com>
2015-08-25 18:25:32 -0500

    Put validation errors along side their form fields
    Some style and standards cleanup for create and edit forms

--- app/views/posts/create-edit-form.blade.php
+++ app/views/posts/create-edit-form.blade.php
@@ -1,11 +1,15 @@
 <div class="form-group @if($errors->has('title')) has-error @endif">
     {{ Form::label('title', 'Title') }}
     {{ Form::text('title', null, ['class' => 'form-control']) }}
+
+    {{ $errors->first('title', '<p class="help-block">:message</p>')}}
 </div>
 
 <div class="form-group @if($errors->has('body')) has-error @endif">
     {{ Form::label('body', 'Body') }}
     {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+
+    {{ $errors->first('body', '<p class="help-block">:message</p>')}}
 </div>
 
-<button class="btn btn-primary">Save</button>
+<button class="btn btn-primary" type="submit">Save</button>
--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-    
-    <h1>{{ $post->title }}</h1>
+    <h1>Edit "{{{ $post->title }}}"</h1>
 
     {{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT')) }}
         @include('posts.create-edit-form')
     {{ Form::close() }}
-
 @stop
